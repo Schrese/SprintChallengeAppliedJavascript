e16f62ef161351efcd59761dc91dcd1d8740465d
Schrese <53787633+Schrese@users.noreply.github.com>
2019-10-04 12:09:57 -0400

    trying to get the tabs to work

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,36 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+    .get(`https://lambda-times-backend.herokuapp.com/topics`)
+    .then(response => {
+        // console.log(response.data);
+        // const tabInfo = response.data;
+        // // tabsCreator(tabInfo);
+
+        response.data.forEach(e => {
+            axios.get(`https://lambda-times-backend.herokuapp.com/topics${e.topics}`)
+            .then(response => {
+                console.log(response);
+                const myVar = response.data;
+                tabsCreator(myVar);
+                tabs.appendChild(tabsCreator(myVar));
+            })
+        })
+        
+    })
+
+const tabs = document.querySelector('.tabs')
+
+function tabsCreator(data) {
+    const tabElement = document.createElement('div');
+
+    tabElement.classList.add('tab');
+
+    tabElement.textContent = data.topics;
+
+    return tabElement;
+
+}
+// tabs.appendChild(tabsCreator(data))
